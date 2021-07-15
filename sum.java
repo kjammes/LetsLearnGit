18dd6cc4d23476e52d87cfc1a8eb2b522dd40b44
jayeshkarli <jayeshkarli@neosoftmail.com>
2021-07-15 17:34:27 +0530

    Initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+class Sum {
+  public static void main(String args[]) {
+    System.out.println("Some method named sum!");
+    int num1 = 10, num2 = 20, sum;
+    sum = num1 + num2;
+    System.out.println("Sum of these numbers are " + sum);
+  }
+}
